94f4573f7ac5c8b0146e8b77f12ed1e63cb3f246
gauravkhanna3007 <gaurav3007@gmail.com>
2023-12-29 23:09:44 +0530

    Repo list in Org

---
+++ files/fetch_repos.py
@@ -0,0 +1,23 @@
+import os
+import requests
+
+ORG_NAME = "github-gk-aks"
+GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
+
+headers = {
+    "Authorization": f"Bearer {GITHUB_TOKEN}",
+    "Accept": "application/vnd.github.v3+json"
+}
+
+# Fetch all repositories in the organization
+url = f"https://api.github.com/orgs/{ORG_NAME}/repos?per_page=100"
+response = requests.get(url, headers=headers)
+repos = response.json()
+
+# Filter out archived repositories
+active_repos = [repo["full_name"] for repo in repos if not repo["archived"]]
+
+# Write the list of repositories to a text file
+with open("repo_list.txt", "w") as f:
+    for repo in active_repos:
+        f.write(f"{repo}\n")
