72a0908e54d5f2c68f07d723ea3bf37671b29b4d
gauravkhanna3007 <gaurav3007@gmail.com>
2023-12-29 23:22:30 +0530

    Repo list in Org

--- files/fetch_repos.py
+++ files/fetch_repos.py
@@ -13,19 +13,24 @@ headers = {
 # Fetch all repositories in the organization
 url = f"https://api.github.com/orgs/{ORG_NAME}/repos?per_page=100"
 response = requests.get(url, headers=headers)
-repos = response.json()
 
-if isinstance(repos, str):
+try:
+    repos = response.json()
+except json.JSONDecodeError:
     try:
-        repos = json.loads(repos)
+        repos = json.loads(response.text)
     except json.JSONDecodeError:
         print("Error decoding JSON. Exiting.")
         exit(1)
+
+# Check if the repos variable is a list (as expected)
+if not isinstance(repos, list):
+    print("Invalid JSON format. Exiting.")
+    exit(1)
 
 # Filter out archived repositories
 active_repos = [repo["full_name"] for repo in repos if not repo["archived"]]
 
 # Write the list of repositories to a text file
-with open("repo_list.txt", "w") as f:
-    for repo in active_repos:
-        f.write(f"{repo}\n")
+with open("repo_list.txt", "w") as file:
+    file.write("\n".join(active_repos))
