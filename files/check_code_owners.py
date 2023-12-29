d6ef1179d006cdeedb73b48d0e03e6e9749dfe82
gauravkhanna3007 <gaurav3007@gmail.com>
2023-12-29 16:44:39 +0530

    Trying to troubleshoot the problem

--- files/check_code_owners.py
+++ files/check_code_owners.py
@@ -2,31 +2,32 @@ import os
 import subprocess
 
 def check_code_owners(repo_name):
-   repo_path = f"{repo_name}"  # Assuming you have a temporary directory for cloning repos
-   code_owners_locations = []
-   # Check for CODEOWNERS file in different locations
-   locations_to_check = [".github/CODEOWNERS", "docs/CODEOWNERS", "CODEOWNERS"]
-   for location in locations_to_check:
-       code_owners_path = os.path.join(repo_path, location)
-       print(f"Checking for CODEOWNERS at: {code_owners_path}")
-       if os.path.exists(code_owners_path):
-           code_owners_locations.append(location)
-           print(f"Found CODEOWNERS at: {code_owners_path}")
-   return code_owners_locations  # Return the locations of CODEOWNERS files
+    repo_path = f"temp_repos/{repo_name}"  # Assuming you have a temporary directory for cloning repos
+    code_owners_locations = []
+    # Check for CODEOWNERS file in different locations
+    locations_to_check = [".github/CODEOWNERS", "docs/CODEOWNERS", "CODEOWNERS"]
+    for location in locations_to_check:
+        code_owners_path = os.path.join(repo_path, location)
+        if os.path.exists(code_owners_path):
+            code_owners_locations.append(location)
+    return code_owners_locations  # Return the locations of CODEOWNERS files
 
 if __name__ == "__main__":
-   repos_file = "files/repo_list.txt"
-   results_file = "results.txt"
-   if not os.path.exists("temp_repos"):
-       os.makedirs("temp_repos")
-   with open(repos_file, "r") as f:
-       repo_names = f.read().splitlines()
-   with open(results_file, "w") as results:
-       for repo_name in repo_names:
-           print(f"Checking repository: {repo_name}")
-           code_owners_locations = check_code_owners(repo_name)
-           print(f"Code Owners Locations : {code_owners_locations}")
-           if code_owners_locations:
-               results.write(f"{repo_name}, {', '.join(code_owners_locations)}\n")
-           else:
-               results.write(f"{repo_name}, Not found\n")
+    repos_file = "files/repo_list.txt"
+    results_file = "results.txt"
+    if not os.path.exists("temp_repos"):
+        os.makedirs("temp_repos")
+
+    with open(repos_file, "r") as f:
+        repo_names = f.read().splitlines()
+
+    with open(results_file, "w") as results:
+        for repo_name in repo_names:
+            # Clone the repository using GITHUB_TOKEN
+            os.system(f"git clone https://github.com/github-gk-aks/{repo_name}.git temp_repos/{repo_name} --depth=1")
+            code_owners_locations = check_code_owners(repo_name)
+            
+            if code_owners_locations:
+                results.write(f"{repo_name}, {', '.join(code_owners_locations)}\n")
+            else:
+                results.write(f"{repo_name}, Not found\n")
