d15d7ffdbb30d900fb095ffcdcab3bbad925d04f
gauravkhanna3007 <gaurav3007@gmail.com>
2023-12-30 13:45:39 +0530

    Modified the python script to add repos from all pages

--- files/fetch_repos.py
+++ files/fetch_repos.py
@@ -7,34 +7,41 @@ github_token = os.environ['GK_PAT']
 
 headers = {
     "Authorization": f"Bearer {github_token}",
-    "Accept": "application/vnd.github.v3+json"
+    "Accept": "application/vnd.github+json",
+    "X-GitHub-Api-Version": "2022-11-28"
 }
 
+all_repositories = []
 # Fetch all repositories in the organization
-url = f"https://api.github.com/orgs/{ORG_NAME}/repos?per_page=100"
-response = requests.get(url, headers=headers)
+for page in range(1, 4):
+    url = f"https://api.github.com/orgs/{ORG_NAME}/repos?per_page=5&page={page}"
+    response = requests.get(url, headers=headers)
 
-try:
-    repos = response.json()
-except json.JSONDecodeError:
     try:
-        repos = json.loads(response.text)
+        repos = response.json()
     except json.JSONDecodeError:
-        print("Error decoding JSON. Exiting.")
+        try:
+            repos = json.loads(response.text)
+        except json.JSONDecodeError:
+            print("Error decoding JSON. Exiting.")
+            print("Response content:")
+            print(response.content.decode())
+            exit(1)
+
+    # Check if the repos variable is a list (as expected)
+    if not isinstance(repos, list):
+        print("Invalid JSON format. Exiting.")
         print("Response content:")
         print(response.content.decode())
         exit(1)
 
-# Check if the repos variable is a list (as expected)
-if not isinstance(repos, list):
-    print("Invalid JSON format. Exiting.")
-    print("Response content:")
-    print(response.content.decode())
-    exit(1)
+    all_repositories.extend(repos)
 
 # Filter out archived repositories
-active_repos = [repo["full_name"] for repo in repos if isinstance(repo, dict) and not repo.get("archived")]
+active_repos = [repo["full_name"] for repo in all_repositories if isinstance(repo, dict) and not repo.get("archived")]
 
 # Write the list of repositories to a text file
 with open("repo_list.txt", "w") as file:
     file.write("\n".join(active_repos))
+
+print(f"Total repositories: {len(active_repos)}")
