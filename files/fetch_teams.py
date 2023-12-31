64b522cb1d2e4820e7eb6153eb5f049168abc889
gauravkhanna3007 <gaurav3007@gmail.com>
2023-12-31 21:41:02 +0530

    Initial commit

---
+++ files/fetch_teams.py
@@ -0,0 +1,68 @@
+import os
+import requests
+import openpyxl
+from openpyxl import Workbook
+from openpyxl.styles import Alignment
+from github import Github
+
+GITHUB_TOKEN = os.environ['GK_PAT']
+ORG_NAME = 'github-gk-aks'
+
+def fetch_teams():
+    url = f'https://api.github.com/orgs/{ORG_NAME}/teams'
+
+    headers = {
+    "Authorization": f"Bearer {GITHUB_TOKEN}",
+    "Accept": "application/vnd.github+json",
+    "X-GitHub-Api-Version": "2022-11-28"
+    }
+    
+    # headers = {'Authorization': f'token {GITHUB_TOKEN}'}
+    response = requests.get(url, headers=headers)
+    teams = response.json()
+    return teams
+
+def fetch_team_members(team_id):
+    url = f'https://api.github.com/teams/{team_id}/members'
+
+    headers = {
+    "Authorization": f"Bearer {GITHUB_TOKEN}",
+    "Accept": "application/vnd.github+json",
+    "X-GitHub-Api-Version": "2022-11-28"
+    }
+
+    # headers = {'Authorization': f'token {GITHUB_TOKEN}'}
+    response = requests.get(url, headers=headers)
+    members = response.json()
+    return members
+
+def main():
+    teams = fetch_teams()
+
+    # Create an Excel workbook
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    sheet.title = 'Teams'
+
+    # Add headers
+    sheet['A1'] = 'Team Name'
+    sheet['B1'] = 'Members'
+
+    # Add data
+    row = 2
+    for team in teams:
+        team_name = team['name']
+        team_id = team['id']
+        members = fetch_team_members(team_id)
+        member_names = ', '.join([member['login'] for member in members])
+
+        sheet[f'A{row}'] = team_name
+        sheet[f'B{row}'] = member_names
+
+        row += 1
+
+    # Save the workbook
+    workbook.save('teams_and_members.xlsx')
+
+if __name__ == '__main__':
+    main()
