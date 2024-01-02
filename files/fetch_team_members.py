304d1a81c4159d89fda608f62a583e7d78005f43
gauravkhanna3007 <gaurav3007@gmail.com>
2024-01-02 23:18:13 +0530

    troubleshooting the JSON decode error

--- files/fetch_team_members.py
+++ files/fetch_team_members.py
@@ -32,6 +32,11 @@ for repository in repositories:
     for team in teams_data:
         team_name = team['slug']
         permission_url = f'{api_base_url}/orgs/{org_name}/teams/{team_name}/repos/{repository}'
+        headers = {
+        "Authorization": f"Bearer {GITHUB_TOKEN}",
+        "Accept": "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28"
+        }
         permission_response = requests.get(permission_url, headers=headers)
         
         # permission_data = permission_response.json()
@@ -53,36 +58,45 @@ for repository in repositories:
         else:
             print(f"Non-200 status code ({permission_response.status_code}) for {permission_url}")     
 
-    # Fetch individual users and their permissions for the repository
-    collaborators_url = f'{api_base_url}/repos/{repository}/collaborators'
-    collaborators_response = requests.get(collaborators_url, headers=headers)
+    # # Fetch individual users and their permissions for the repository
+    # collaborators_url = f'{api_base_url}/repos/{repository}/collaborators'
+    # headers = {
+    # "Authorization": f"Bearer {GITHUB_TOKEN}",
+    # "Accept": "application/vnd.github+json",
+    # "X-GitHub-Api-Version": "2022-11-28"
+    # }
+    # collaborators_response = requests.get(collaborators_url, headers=headers)
    
+    # if collaborators_response.status_code == 200:
+    #      collaborators_data = collaborators_response.json()
 
-    if collaborators_response.status_code == 200:
-         collaborators_data = collaborators_response.json()
+    # for collaborator in collaborators_data:
+    #     username = collaborator['login']
+    #     permission_url = f'{api_base_url}/repos/{repository}/collaborators/{username}/permissions'
+    #     headers = {
+    #     "Authorization": f"Bearer {GITHUB_TOKEN}",
+    #     "Accept": "application/vnd.github+json",
+    #     "X-GitHub-Api-Version": "2022-11-28"
+    #     }
+    #     permission_response = requests.get(permission_url, headers=headers)
 
-    for collaborator in collaborators_data:
-        username = collaborator['login']
-        permission_url = f'{api_base_url}/repos/{repository}/collaborators/{username}/permissions'
-        permission_response = requests.get(permission_url, headers=headers)
+    #     # permission_data = permission_response.json()
+    #     if permission_response.status_code == 200:
+    #         try:
+    #             permission_data = permission_response.json()
+    #             # Iterate through permissions and append to Excel worksheet
+    #             permission = permission_data.get('permission')
+    #             worksheet.append([repository, f'User: {username}', f'Permission: {permission}'])
+    #         except requests.exceptions.JSONDecodeError as e:
+    #             print(f"Error decoding JSON for {permission_url}: {e}")
+    #             print(f"Response content: {permission_response.content}")
+    #             continue
 
-        # permission_data = permission_response.json()
-        if permission_response.status_code == 200:
-            try:
-                permission_data = permission_response.json()
-                # Iterate through permissions and append to Excel worksheet
-                permission = permission_data.get('permission')
-                worksheet.append([repository, f'User: {username}', f'Permission: {permission}'])
-            except requests.exceptions.JSONDecodeError as e:
-                print(f"Error decoding JSON for {permission_url}: {e}")
-                print(f"Response content: {permission_response.content}")
-                continue
-
-            if 'permission' not in permission_data:
-                print(f"Invalid response for {permission_url}. Skipping...")
-                continue  
-        else:
-            print(f"Non-200 status code ({permission_response.status_code}) for {permission_url}")
+    #         if 'permission' not in permission_data:
+    #             print(f"Invalid response for {permission_url}. Skipping...")
+    #             continue  
+    #     else:
+    #         print(f"Non-200 status code ({permission_response.status_code}) for {permission_url}")
 
 # Save the Excel file
 workbook.save('teams_members_permissions.xlsx')
