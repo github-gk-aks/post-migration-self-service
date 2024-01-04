4021d75199e4c6b8a430e92b73bbeb7ad72fdec3
gauravkhanna3007 <gaurav3007@gmail.com>
2024-01-04 13:34:01 +0530

    Still working on it

--- files/fetch_team_members.py
+++ files/fetch_team_members.py
@@ -63,44 +63,44 @@ for repository in repositories:
             print(f"Non-200 status code ({permission_response.status_code}) for {permission_url}")     
 
     # # Fetch individual users and their permissions for the repository
-    # collaborators_url = f'{api_base_url}/repos/{repository}/collaborators'
-    # headers = {
-    # "Authorization": f"Bearer {GITHUB_TOKEN}",
-    # "Accept": "application/vnd.github+json",
-    # "X-GitHub-Api-Version": "2022-11-28"
-    # }
-    # collaborators_response = requests.get(collaborators_url, headers=headers)
+    collaborators_url = f'{api_base_url}/repos/{repository}/collaborators?affiliation=direct'
+    headers = {
+    "Authorization": f"Bearer {GITHUB_TOKEN}",
+    "Accept": "application/vnd.github+json",
+    "X-GitHub-Api-Version": "2022-11-28"
+    }
+    collaborators_response = requests.get(collaborators_url, headers=headers)
    
-    # if collaborators_response.status_code == 200:
-    #      collaborators_data = collaborators_response.json()
+    if collaborators_response.status_code == 200:
+         collaborators_data = collaborators_response.json()
 
-    # for collaborator in collaborators_data:
-    #     username = collaborator['login']
-    #     permission_url = f'{api_base_url}/repos/{repository}/collaborators/{username}/permissions'
-    #     headers = {
-    #     "Authorization": f"Bearer {GITHUB_TOKEN}",
-    #     "Accept": "application/vnd.github+json",
-    #     "X-GitHub-Api-Version": "2022-11-28"
-    #     }
-    #     permission_response = requests.get(permission_url, headers=headers)
+    for collaborator in collaborators_data:
+        username = collaborator['login']
+        permission_url = f'{api_base_url}/repos/{repository}/collaborators/{username}/permissions'
+        headers = {
+        "Authorization": f"Bearer {GITHUB_TOKEN}",
+        "Accept": "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28"
+        }
+        permission_response = requests.get(permission_url, headers=headers)
 
-    #     # permission_data = permission_response.json()
-    #     if permission_response.status_code == 200:
-    #         try:
-    #             permission_data = permission_response.json()
-    #             # Iterate through permissions and append to Excel worksheet
-    #             permission = permission_data.get('permission')
-    #             worksheet.append([repository, f'User: {username}', f'Permission: {permission}'])
-    #         except requests.exceptions.JSONDecodeError as e:
-    #             print(f"Error decoding JSON for {permission_url}: {e}")
-    #             print(f"Response content: {permission_response.content}")
-    #             continue
+        # permission_data = permission_response.json()
+        if permission_response.status_code == 200:
+            try:
+                permission_data = permission_response.json()
+                # Iterate through permissions and append to Excel worksheet
+                permission = permission_data.get('role_name')
+                worksheet.append([repository, f'User: {username}', f'Permission: {permission}'])
+            except requests.exceptions.JSONDecodeError as e:
+                print(f"Error decoding JSON for {permission_url}: {e}")
+                print(f"Response content: {permission_response.content}")
+                continue
 
-    #         if 'permission' not in permission_data:
-    #             print(f"Invalid response for {permission_url}. Skipping...")
-    #             continue  
-    #     else:
-    #         print(f"Non-200 status code ({permission_response.status_code}) for {permission_url}")
+            if 'permission' not in permission_data:
+                print(f"Invalid response for {permission_url}. Skipping...")
+                continue  
+        else:
+            print(f"Non-200 status code ({permission_response.status_code}) for {permission_url}")
 
 # Save the Excel file
 workbook.save('teams_members_permissions.xlsx')
