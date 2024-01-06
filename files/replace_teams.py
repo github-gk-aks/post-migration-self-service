b1dc6637fb83c99355d6644f5270028dbddf0576
gauravkhanna3007 <gaurav3007@gmail.com>
2024-01-06 15:38:40 +0530

    Initial commit for replacing teams in codeowners file

--- files/replace_teams.py
+++ files/replace_teams.py
@@ -5,6 +5,10 @@ from github import Github
 # Read repository information from the first Excel file
 workbook = openpyxl.load_workbook('data/CodeOwners-File-Location.xlsx')
 sheet = workbook.active
+
+codeowners_file_location_path = 'data/CodeOwners-File-Location.xlsx'
+teams_to_replace_path = 'data/CodeOwners-Team-Replacement.xlsx'
+
 for row in sheet.iter_rows(min_row=2, values_only=True):
     org_name, repo_name, codeowners_path = row
 
@@ -17,7 +21,7 @@ for row in sheet.iter_rows(min_row=2, values_only=True):
     os.chdir(repo_path)
 
     # Read teams to be replaced from the second Excel file
-    with openpyxl.load_workbook('data/CodeOwners-Team-Replacement.xlsx') as teams_workbook:
+    with openpyxl.load_workbook(teams_to_replace_path) as teams_workbook:
         teams_sheet = teams_workbook.active
         for team_row in teams_sheet.iter_rows(min_row=2, values_only=True):
             search_team, replace_team = team_row
