1ee2cc3bd02e50922cfd02b3ad6192a44081e978
gauravkhanna3007 <gaurav3007@gmail.com>
2024-01-06 20:40:44 +0530

    Trying a different approach

---
+++ files/read_teams.py
@@ -0,0 +1,16 @@
+import openpyxl
+import json
+
+teams = []
+
+workbook = openpyxl.load_workbook('data/CodeOwners-Team-Replacement.xlsx')
+sheet = workbook.active
+
+for row in sheet.iter_rows(min_row=2, values_only=True):
+    teams.append({'search': row[0], 'replace': row[1]})
+
+# Convert teams to a JSON string
+teams_json = json.dumps(teams)
+
+workbook.close()
+print(f"::set-output name=teams::{teams_json}")
---
+++ files/replace_teams.py
@@ -0,0 +1,18 @@
+import os
+import json
+
+repo_path = '.'
+codeowners_location = os.environ.get('CODEOWNERS_LOCATION')
+
+teams_to_replace = json.loads(os.environ.get('TEAMS', '[]'))
+
+# Read and replace teams in the CODEOWNERS file
+codeowners_path = os.path.join(repo_path, codeowners_location)
+with open(codeowners_path, 'r') as file:
+    content = file.read()
+
+for team in teams_to_replace:
+    content = content.replace(team['search'], team['replace'])
+
+with open(codeowners_path, 'w') as file:
+    file.write(content)
